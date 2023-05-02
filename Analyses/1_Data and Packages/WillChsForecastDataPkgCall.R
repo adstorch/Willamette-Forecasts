ed09443a4da1d0f16e8dc0783fa72b17e2518787
Adam J Storch <Adam.J.STORCH@odfw.oregon.gov>
2023-05-02 09:24:38 -0700

    reformat data and package code

--- Analyses/1_Data and Packages/WillChsForecastDataPkgCall.R
+++ Analyses/1_Data and Packages/WillChsForecastDataPkgCall.R
@@ -1,4 +1,4 @@
-# call packages -----------------------------------------------------------
+# call packages ----------------------------------------------------------------
 packages <- c("openxlsx",
               "ggplot2",
               "R2jags",
@@ -10,35 +10,44 @@ if (!require(install.load)) {
 
 install.load::install_load(packages)
 
-# call raw data -----------------------------------------------------------
+# call raw data ----------------------------------------------------------------
 ## input file path
 inPath <- "Input\\Input Data\\WillClackRawData.xlsx"
 
 ## Willamette return data
-willChsRet.dat <- read.xlsx(inPath,
-                            sheet = 1,
-                            colNames = TRUE)
+willChsRet.dat <- read.xlsx(
+  inPath,
+  sheet = 1,
+  colNames = TRUE
+)
 
 ## Willamette covariate data
-willChsCov.dat <- read.xlsx(inPath,
-                            sheet = 2,
-                            colNames = TRUE)
+willChsCov.dat <- read.xlsx(
+  inPath,
+  sheet = 2,
+  colNames = TRUE
+)
 
 ## Willamette HW proportion data
-willChsHWprop.dat <- read.xlsx(inPath,
-                               sheet = 3,
-                               colNames = TRUE)
+willChsHWprop.dat <- read.xlsx(
+  inPath,
+  sheet = 3,
+  colNames = TRUE
+)
 
 ## Clackamas return data
 ### call raw data
-clackChsRet.dat <- read.xlsx(inPath,
-                             sheet = 4,
-                             colNames = TRUE)
+clackChsRet.dat <- read.xlsx(
+  inPath,
+  sheet = 4,
+  colNames = TRUE
+)
 
 ### sum diagonals to calculate total returns in year n
 clackChsTot.dat <- data.frame(
   total_clack=sapply(
-    1:nrow(clackChsRet.dat[,2:5]
+    1:nrow(
+      clackChsRet.dat[,2:5]
     )+1,
     function(j) sum(
       clackChsRet.dat[,2:5][row(
@@ -54,5 +63,8 @@ clackChsTot.dat <- data.frame(
 clackChsTot.dat[1:3,] <- 2200
 
 ### create a combined ('manipulated') data frame
-clackChsManip.dat <- cbind(clackChsRet.dat,clackChsTot.dat)
+clackChsManip.dat <- cbind(
+  clackChsRet.dat,
+  clackChsTot.dat
+)
 
