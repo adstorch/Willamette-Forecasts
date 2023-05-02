c0fff86951f0dbcf60b1897632ab2ecfefb7db60
Adam J Storch <Adam.J.STORCH@odfw.oregon.gov>
2023-05-02 09:55:43 -0700

    reformat age-6 code

--- Analyses/5_Age 6/willChsForecastScript_age6.R
+++ Analyses/5_Age 6/willChsForecastScript_age6.R
@@ -1,4 +1,4 @@
-# data manipulation -------------------------------------------------------
+# data manipulation ------------------------------------------------------------
 ## generate a data frame to fit model
 willAge6Fit.dat <- tail(
   head(
@@ -18,16 +18,16 @@ willAge6Pred.dat <- tail(
   1
 )
 
-# fit model and generate prediction ---------------------------------------
+# fit model and generate prediction --------------------------------------------
 ## create character string defining current model
 ## (string will have to be changed manually if parameterization changes)
 willAge6Mod.name <- "Willamette Age-6"
 
 ## create data list to fit model and generate prediction
 willAge6Mod.dat <- list(
-  ratio_65yr=willAge6Fit.dat,
+  ratio_65yr = willAge6Fit.dat,
   
-  age5_col=willAge6Pred.dat,
+  age5_col = willAge6Pred.dat,
   
   nObs=length(
     willAge6Fit.dat
@@ -51,15 +51,18 @@ willAge6Mod.dat <- list(
       willChsHWprop.dat,
       -1
     )[,4]
-  )+1
+  ) + 1
 )
 
-## define initial values (user defined initial values are not accepted by jags.parallel()-default is to let jags estimate initial values; otherwise use jags() and specify initial values as below)
+## define initial values
+## (user defined initial values are not accepted by jags.parallel()-
+## default is to let jags estimate initial values; otherwise use jags() and
+## specify initial values as below)
 inits.willAge6 <- function()
 {
   list(
-    muRatio_65yr=rnorm(1,0,1),
-    sig=1
+    muRatio_65yr = rnorm(1,0,1),
+    sig = 1
   )
 }
 
@@ -96,9 +99,9 @@ cat('
     r <- 1/inv.r
     X0 ~ dnorm(Y1, 0.001)
     
-    predHat_willAge6 <- pred_willAge6-(pred_willAge6*EY[nHWobs])
+    predHat_willAge6 <- pred_willAge6 - (pred_willAge6 * EY[nHWobs])
   }',
-  file={willAge6.mod <- tempfile()})
+    file={willAge6.mod <- tempfile()})
 
 ## define parameters to monitor
 params.willAge6 <- c(
@@ -131,7 +134,7 @@ fit.willAge6 <- jags.parallel(
 
 stop <- Sys.time()
 duration <- stop-start
-# review and summarize output ---------------------------------------------
+# review and summarize output --------------------------------------------------
 ## review bugs object
 fit.willAge6
 
@@ -147,10 +150,10 @@ pred.mcmc.willAge6 <- mcmc.willAge6[, paste(
 ## summarize output for current prediction
 ### create data frame to store output
 willAge6.pred.out <- data.frame(
-  model=character(),
-  mean.pred.willAge6=numeric(),
-  lwrHDI.willAge6=numeric(),
-  UprHDI.willAge6=numeric(),
+  model = character(),
+  mean.pred.willAge6 = numeric(),
+  lwrHDI.willAge6 = numeric(),
+  UprHDI.willAge6 = numeric(),
   stringsAsFactors = FALSE
 )
 
@@ -168,7 +171,7 @@ willAge6.pred.out[1,] <- c(
   round(
     hdi(
       pred.mcmc.willAge6,
-      credMass=0.95
+      credMass = 0.95
     )[1],
     0
   ),
@@ -176,7 +179,7 @@ willAge6.pred.out[1,] <- c(
   round(
     hdi(
       pred.mcmc.willAge6,
-      credMass=0.95
+      credMass = 0.95
     )[2],
     0
   )
@@ -187,10 +190,10 @@ predHat.mcmc.willAge6 <- mcmc.willAge6[, "predHat_willAge6"]
 
 #### create data frame to store output for hatchery predictions
 willAge6.predHat.out <- data.frame(
-  model=character(),
-  mean.pred.willAge6=numeric(),
-  lwrHDI.willAge6=numeric(),
-  UprHDI.willAge6=numeric(),
+  model = character(),
+  mean.pred.willAge6 = numeric(),
+  lwrHDI.willAge6 = numeric(),
+  UprHDI.willAge6 = numeric(),
   stringsAsFactors = FALSE
 )
 
@@ -211,7 +214,7 @@ willAge6.predHat.out[1,] <- c(
   round(
     hdi(
       predHat.mcmc.willAge6,
-      credMass=0.95
+      credMass = 0.95
     )[1],
     0
   ),
@@ -219,7 +222,7 @@ willAge6.predHat.out[1,] <- c(
   round(
     hdi(
       predHat.mcmc.willAge6,
-      credMass=0.95
+      credMass = 0.95
     )[2],
     0
   )
