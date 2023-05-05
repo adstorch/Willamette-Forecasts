0c31365c1a86b09bd6d7c23f5ec424e5542bb012
Adam J Storch <Adam.J.STORCH@odfw.oregon.gov>
2023-05-05 07:21:28 -0700

    update

--- Analyses/1_Data and Packages/WillChsForecastDataPkgCall.R
+++ Analyses/1_Data and Packages/WillChsForecastDataPkgCall.R
@@ -13,19 +13,21 @@ install.load::install_load(packages)
 # call exogenous functions -----------------------------------------------------
 source("Exogenous functions\\round_fun.R")
 
-
-# call data --------------------------------------------------------------------
+# load existing file -----------------------------------------------------------
 load(file='Input\\Input Data\\willChsRet.rda')
 load(file='Input\\Input Data\\clackChsRet.rda')
 
-## set current return year (this has to be entered manually)
+# update existing file(s) ------------------------------------------------------
+## set current return year (this applies to all datasets to be updated)
 curr_year <- 2023
 
+### Willamette return data
+#### create time variable
 time <- data.frame(brd_yr = curr_year - 2,
                    mig_yr = curr_year
 )
 
-## enter return estimates from current run reconstruction
+#### enter return estimates from current run reconstruction
 age3_col <- 2123
 age4_col <- 40123
 age5_col <- 20123
@@ -33,7 +35,7 @@ age6_col <- 50
 age3_will <- 1572
 age2_will <- 3000
 
-## append current years to loaded data frame
+#### append current year to loaded data frame
 willChsRet.dat <- rbind(
   willChsRet.dat,
   setNames(
@@ -45,7 +47,7 @@ willChsRet.dat <- rbind(
   )
 )
 
-## insert new values into data frame
+#### insert new values into data frame
 willChsRet.dat[nrow(willChsRet.dat) - 1, 3] = age3_col
 willChsRet.dat[nrow(willChsRet.dat) - 2, 4] = age4_col
 willChsRet.dat[nrow(willChsRet.dat) - 3, 5] = age5_col
@@ -53,7 +55,7 @@ willChsRet.dat[nrow(willChsRet.dat) - 4, 6] = age6_col
 willChsRet.dat[nrow(willChsRet.dat) - 1, 7] = age3_will
 willChsRet.dat[nrow(willChsRet.dat), 8] = age2_will
 
-## save backup of updated .rda
+#### save backup of updated .rda
 save(willChsRet.dat, file=paste('Input\\Input Data\\Backup\\Return\\',
                                 'Willamette\\',
                                 curr_year,
@@ -61,13 +63,10 @@ save(willChsRet.dat, file=paste('Input\\Input Data\\Backup\\Return\\',
                                 sep = "")
 )
 
-## save working updated .rda (this will overwrite existing file)
+#### save working updated .rda (this will overwrite existing file)
 save(willChsRet.dat, file='Input\\Input Data\\willChsRet.rda')
 
-## create sum variable for current analysis
-willChsRet.dat$sum23 <- 
-  willChsRet.dat$age3_col +
-  willChsRet.dat$age2_will
+
 
 ## call raw data ---------------------------------------------------------------
 ## input file path
@@ -82,14 +81,14 @@ willChsRet.dat$sum23 <-
 # # 
 # save(willChsRet.dat, file='Input\\Input Data\\willChsRet.rda')
 
-## Willamette covariate data
-willChsCov.dat <- read.xlsx(
-  inPath,
-  sheet = 2,
-  colNames = TRUE
-)
+# ## Willamette covariate data
+# willChsCov.dat <- read.xlsx(
+#   inPath,
+#   sheet = 2,
+#   colNames = TRUE
+# )
 
-save(willChsCov.dat, file='Input\\Input Data\\willChsCov.rda')
+# save(willChsCov.dat, file='Input\\Input Data\\willChsCov.rda')
 
 # ## Willamette HW proportion data
 # clackChsRet.dat <- read.xlsx(
@@ -100,16 +99,19 @@ save(willChsCov.dat, file='Input\\Input Data\\willChsCov.rda')
 # 
 # save(clackChsRet.dat, file='Input\\Input Data\\clackChsRet.rda')
 
-## Clackamas return data
+### Clackamas return data
+#### create time variable
+time.clack <- data.frame(
+  brd_yr = curr_year - 3
+  )
+
 #### enter return estimates from current run reconstruction
 age3_clack <- 290
 age4_clack <- 5796
 age5_clack <- 346
 age6_clack <- 0
 
-time.clack <- data.frame(brd_yr = curr_year - 3)
-
-## append current years to loaded data frame
+#### append current years to loaded data frame
 clackChsRet.dat <- rbind(
   clackChsRet.dat,
   setNames(
@@ -121,13 +123,13 @@ clackChsRet.dat <- rbind(
   )
 )
 
-## insert new values into data frame
+#### insert new values into data frame
 clackChsRet.dat[nrow(clackChsRet.dat), 2] = age3_clack
 clackChsRet.dat[nrow(clackChsRet.dat) - 1, 3] = age4_clack
 clackChsRet.dat[nrow(clackChsRet.dat) - 2, 4] = age5_clack
 clackChsRet.dat[nrow(clackChsRet.dat) - 3, 5] = age6_clack
 
-## save backup of updated .rda
+#### save backup of updated .rda
 save(clackChsRet.dat, file=paste('Input\\Input Data\\Backup\\Return\\',
                                 'Clackamas\\',
                                 curr_year,
@@ -135,9 +137,17 @@ save(clackChsRet.dat, file=paste('Input\\Input Data\\Backup\\Return\\',
                                 sep = "")
 )
 
-## save working updated .rda (this will overwrite existing file)
+#### save working updated .rda (this will overwrite existing file)
 save(clackChsRet.dat, file='Input\\Input Data\\clackChsRet.rda')
 
+# manipulated existing or updated dataset --------------------------------------
+## Willamette return
+### create sum variable for current analysis
+willChsRet.dat$sum23 <- 
+  willChsRet.dat$age3_col +
+  willChsRet.dat$age2_will
+
+## Clackamas return
 ### sum diagonals to calculate total returns in year n
 clackChsTot.dat <- data.frame(
   total_clack=sapply(
@@ -162,3 +172,4 @@ clackChsRet.dat <- cbind(
   clackChsRet.dat,
   clackChsTot.dat
 )
+
